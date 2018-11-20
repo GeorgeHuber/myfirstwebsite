bbc63f1d4d57c053fb92832e157c9b693ddccb9d
georgehuber <georgehuber8@gmail.com>
2018-11-20 15:15:16 -0600

    Initial Commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function sayHello(){
+	for (i=0;i<10;i++){alert("Hello #"+(i+1));}
+	document.getElementById("par1").style="display: block";
+}
+function sayThank(){
+alert("Thank You "+document.getElementById("name").value+"!")
+if (document.getElementById("name").value=="George"){
+document.getElementById("body3").style="display: block";
+}
+else{
+alert("You do not have access..."};
+
+
+
+
+}
+function final(){x=1;
+for (i=0, i<100, i++){x=x*2; document.getElementByID("final").size=x;}
+
+}
