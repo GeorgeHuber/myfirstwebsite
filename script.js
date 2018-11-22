b9283ab388c1e2954e3aada1065f92e36c51c20e
georgehuber <georgehuber8@gmail.com>
2018-11-22 08:14:54 -0600

    added final function betae

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function sayHello(){
 for (i=0;i<10;i++){alert("Hello #"+(i+1));}
 	document.getElementById("par1").style="display: block";
 }
-
+B
 function sayThank(){
 	alert("Thank You "+document.getElementById("name").value+"!");
 	if(document.getElementById("name").value=="George"){
@@ -10,10 +10,15 @@ function sayThank(){
 	} else{
 	alert("You do not have access...")}
 }
+function doSetTimeout(x) {
+  setTimeout(function(){alert("thefunc"); document.getElementById("final").style="font-size: "+x+"px";},2000);}
 
 function final(){
-conf=confirm("Do you want to continue?");
+var x=1; conf=confirm("Do you want to continue?");
+if (conf==true) {
+        for (i=0; i<100; i++){doSetTimeout(x); x=x*2;}
 
-
-
+    } else {
+        alert("Goodbye");
+    }
 }
