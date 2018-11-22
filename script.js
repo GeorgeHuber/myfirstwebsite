2d92a7d9df097a5c3b173a5e944becc00aa636d2
georgehuber <georgehuber8@gmail.com>
2018-11-21 19:58:57 -0600

    Solved all errors and added 2nd page

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 function sayHello(){
-	for (i=0;i<10;i++){alert("Hello #"+(i+1));}
+for (i=0;i<10;i++){alert("Hello #"+(i+1));}
 	document.getElementById("par1").style="display: block";
 }
+
 function sayThank(){
-alert("Thank You "+document.getElementById("name").value+"!")
-if (document.getElementById("name").value=="George"){
-document.getElementById("body3").style="display: block";
+	alert("Thank You "+document.getElementById("name").value+"!");
+	if(document.getElementById("name").value=="George"){
+		document.getElementById("body3").style="display: block";
+	} else{
+	alert("You do not have access...")}
 }
-else{
-alert("You do not have access..."};
+
+function final(){
+conf=confirm("Do you want to continue?");
 
 
-
-
-}
-function final(){x=1;
-for (i=0, i<100, i++){x=x*2; document.getElementByID("final").size=x;}
 
 }
