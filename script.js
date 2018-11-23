7a6c1e657cd8467203d886ec81cb04f9964c270b
georgehuber <georgehuber8@gmail.com>
2018-11-22 20:34:14 -0600

    finished final function

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function sayHello(){
 for (i=0;i<10;i++){alert("Hello #"+(i+1));}
 	document.getElementById("par1").style="display: block";
 }
-B
+
 function sayThank(){
 	alert("Thank You "+document.getElementById("name").value+"!");
 	if(document.getElementById("name").value=="George"){
@@ -10,15 +10,27 @@ function sayThank(){
 	} else{
 	alert("You do not have access...")}
 }
-function doSetTimeout(x) {
-  setTimeout(function(){alert("thefunc"); document.getElementById("final").style="font-size: "+x+"px";},2000);}
+
 
 function final(){
-var x=1; conf=confirm("Do you want to continue?");
-if (conf==true) {
-        for (i=0; i<100; i++){doSetTimeout(x); x=x*2;}
+	conf=confirm("Do you want to continue?");
+	if (conf==true){
+		var i = 1;                     //  set your counter to 
 
-    } else {
-        alert("Goodbye");
-    }
+		function myLoop () {           //  create a loop 
+   			setTimeout(function () {    //  call a 3s setT
+      			
+			document.getElementById("final").style="font-size: "+i+"px";//  your code here
+      			i++;
+			      if (i < 1000) {  
+        				myLoop(i); 
+      } }, 50)                       //  ..  setTimeout()
+   
 }
+
+myLoop();                       		
+		
+    	} else {alert("Goodbye");
+    	}
+}
+
